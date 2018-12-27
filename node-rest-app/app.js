63720df6fbba311fb2719206c48fed256215dfe4
Cristian Marius <cristian.marius.florin@gmail.com>
2018-12-27 16:22:16 +0100

    fixed customers route. works with curl requests, removed the "bluebird" promisify as its borken

--- node-rest-app/app.js
+++ node-rest-app/app.js
@@ -5,10 +5,11 @@ const users = require('./routes/users');
 const bodyParser = require('body-parser');
 const mongoose = require('./config/database');
 const jwt = require('jsonwebtoken');
-const Promise = require("bluebird");
 const app = express();
+const Promise = require("bluebird");
+//bluebird seems to not work. used if
 app.set('secretKey', 'this_should_be_generated_in_an_env_var'); // jwt secret token
-// connection to mongodb
+//connection to mongodb
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(logger('dev'));
 //app.use(bodyParser.urlencoded({extended: true}));
@@ -16,31 +17,39 @@ app.use(bodyParser.json());
 app.get('/', function(req, res){
 res.json({"message": "Landing Page"});
 });
-// public route
+//public route
 app.use('/users', users);
-// private route
+//private route
 app.use('/customers', validate_user, movies);
-
 function validate_user(req, res, next) {
-  const jwtVerifyAsync = Promise.promisify(jwt.verify, jwt);
-  jwtVerifyAsync.verify(req.headers['x-access-token'], req.app.get('secretKey'))
-     .then(function(decoded){
+  // promise version that does not work
+  //const jwtVerifyAsync = Promise.promisify(jwt.verify, {context:jwt});
+  //jwtVerifyAsync.verify(req.headers['x-access-token'], req.app.get('secretKey'))
+  //   .then(function(decoded){
+  //    // add user id to request
+  //    req.body.userId = decoded.id;
+  //    next();
+  //  })
+  //  .catch(function(err){
+  //    res.json({status:409, message: err.message, data:null});
+  //  });
+   jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), function(err, decoded) {
+    if (err) {
+      res.json({status:"error", message: err.message, data:null});
+    }else{
       // add user id to request
       req.body.userId = decoded.id;
       next();
-    })
-    .catch(function(err){
-      res.json({status:409, message: err.message, data:null});
-    });
+    }
+  }); 
 }
-
-// handle 404 error
+//handle 404 error
 app.use(function(req, res, next) {
  let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-// handle errors
+//handle errors
 app.use(function(err, req, res, next) {
   console.log(err);
   switch(err.status){
