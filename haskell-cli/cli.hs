eb56b03ebd4bdac360011711ddc2f18e9358708c
Cristian Marius <cristian.marius.florin@gmail.com>
2018-12-28 00:39:37 +0100

    todo: read token, use it for requests, maybe show output nice

--- haskell-cli/cli.hs
+++ haskell-cli/cli.hs
@@ -8,8 +8,9 @@
    --package aeson
 -}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE Arrows #-}
+{-# LANGUAGE DeriveGeneric #-}
 
+import GHC.Generics
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Char8 as C8
@@ -18,9 +19,11 @@ import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types.Status  (statusCode)
 import System.FilePath
+import System.IO
 import Data.String ( fromString )
 import Control.Monad (forM, liftM)
-import Data.Aeson (Value, encode, object, (.=))
+import Control.Monad.Catch
+import Data.Aeson (Value, FromJSON, encode, object, (.=))
 import Data.Text
 
 -- data for Json Types
@@ -28,61 +31,28 @@ data UserLogin = UserLogin {
   status    :: Int
 , message   :: Text
 , datafield :: Maybe UserData
-} deriving Show
+} deriving (Show, Generic)
 --
 data UserData = UserData {
   user :: User
 , token :: String
-} deriving Show
+} deriving (Show, Generic)
 --
 data User = User {
   id        :: Text
 , username  :: Text
-} deriving Show
+} deriving (Show, Generic)
 
 data Customer = Customer {
   name  :: Text
 , email :: Text
 , phone :: Text
-} deriving Show
---
---
---instance FromJSON UserLogin where
---    parseJSON (Object v) =
---        Photo <$> v .: "status"
---              <*> v .: "message"
---              <*> v .: "data"
---    parseJSON _ = mzero
---
---instance FromJSON UserData where
---    parseJSON (Object v) =
---        Photo <$> v .: "_id"
---              <*> v .: "username"
---    parseJSON _ = mzero
---
---instance FromJSON Customer where
---    parseJSON (Object v) =
---        Photo <$> v .: "name"
---              <*> v .: "email"
---              <*> v .: "phone"
---    parseJSON _ = mzero    
---
---  extractToken :: (UserLogin u) => String
---  extractToken (UserLogin status message datafield) =
---    case datafield of
---      Just (UserData user token) -> show token
---      Nothing -> show "wrong datatype"
---  
---  extractUserId :: (UserLogin u) => String
---  extractUserId (UserLogin status message datafield) =
---    case datafield of
---decodeJSONField :: String -> String -> Maybe String
---decodeJSONField json field = do
---  result <- decode json
---  flip parseMaybe result $ \obj -> do
---    fld <- obj .: field
---    return (show fld) 
+} deriving (Show, Generic)
 
+instance FromJSON UserLogin
+instance FromJSON UserData
+instance FromJSON User
+instance FromJSON Customer
 
 register :: String
 register = "http://localhost:3000/users/register"
@@ -99,68 +69,8 @@ customerSearch src = "http://localhost:3000/customers/" ++ src
 tokenPath :: FilePath
 tokenPath = "./tmp/.jwt-token"
 
-main :: IO ()
-main = do
-  manager <- newManager tlsManagerSettings
-  loopOver manager
---  let requestObject = object
---            [ "username" .= ("Alice" :: String)
---            , "password" .= ("Boring" :: String)
---            ]
---  initialRequest <- parseRequest "http://localhost:3000/users/register"
---  let request = initialRequest
---          { method = "POST"
---          , requestBody = RequestBodyLBS $ encode requestObject
---          , requestHeaders =
---              [ ("Content-Type", "application/json; charset=utf-8")
---              ]
---          }
---  response <- httpLbs request manager
---  putStrLn $ "The status code was: "
---          ++ show (statusCode $ responseStatus response)
---  L8.putStrLn $ responseBody response
---
---
---  let requestObject2 = object
---            [ "username" .= ("Alice" :: String)
---            , "password" .= ("Boring" :: String)
---            ]
---  secondRequest <- parseRequest "http://localhost:3000/users/authenticate"
---  let request2 = secondRequest
---          { method = "POST"
---          , requestBody = RequestBodyLBS $ encode requestObject2
---          , requestHeaders =
---              [ ("Content-Type", "application/json; charset=utf-8")
---              ]
---          }
---  response2 <- httpLbs request2 manager
---  putStrLn $ "The status code was: "
---          ++ show (statusCode $ responseStatus response2)
---  L8.putStrLn $ responseBody response2
---
---
---  let requestObject3 = object
---            [ "id" .= ("5c2255e15dedab2a1c888082" :: String)
---            , "name" .= ("Boring" :: String)
---            , "email" .= ("test"  :: String)
---            , "phone" .= ("1d1a"  :: String)
---            ]
---  thirdRequest <- parseRequest "http://localhost:3000/customers/authenticate"
---  --let token = 
---  let request3 = thirdRequest
---          { method = "POST"
---          , requestBody = RequestBodyLBS $ encode requestObject3
---          , requestHeaders =
---              [ ("Content-Type", "application/json; charset=utf-8"),
---                ("x-access-token", "key to be extracted")
---              ]
---          }
---  response3 <- httpLbs request3 manager
---  putStrLn $ "The status code was: "
---          ++ show (statusCode $ responseStatus response3)
---  L8.putStrLn $ responseBody response3
-  putStrLn $ "Ok Bye."
-
+emptyToken :: String
+emptyToken = "whatever"
 
 -- build user request
 requestUser :: String -> String -> Value
@@ -182,53 +92,87 @@ requestCustomer name email phone = object
 -- to be m Request
 -- thus I cut corner and dumped IO()
 -- input: manager, url, token or empty string, body object
-buildPOSTRequest :: Manager -> String -> String -> Value -> IO ()
-buildPOSTRequest manager url tkn obj =
+buildPOSTRequest :: (MonadThrow m) => String -> String -> Value -> m Request
+buildPOSTRequest url tkn obj =
   do
     req <- parseRequest url
-    let freq = req {method = "POST"
-      ,requestBody = RequestBodyLBS $ encode obj
-      ,requestHeaders =
+    let fullReq = req {method = "POST"
+      , requestBody = RequestBodyLBS $ encode obj
+      , requestHeaders =
         [ ("Content-Type", "application/json; charset=utf-8")
         , ("x-access-token", C8.pack tkn)
         ]
       }
-    response <- httpLbs freq manager
-    putStrLn $ "The status code was: "
-          ++ show (statusCode $ responseStatus response)
-    L8.putStrLn $ responseBody response
+    return fullReq
+
+
+getResponse:: Request -> Manager -> IO BS.ByteString
+getResponse req manager = responseBody <$> httpLbs req manager
+
+-- get request in the same manner
+buildGETRequest :: (MonadThrow m) => String -> String -> m Request
+buildGETRequest url tkn =
+  do
+    req <- parseRequest url
+    let fullReq = req {method = "GET"
+      , requestHeaders =
+        [("x-access-token", C8.pack tkn)]
+      }
+    return fullReq
+--    
+--    response <- httpLbs fullReq manager
+--    putStrLn $ "The status code was: "
+--          ++ show (statusCode $ responseStatus response)
+--    L8.putStrLn $ responseBody response
 
 -- CLI loop
 loopOver :: Manager -> IO()
 loopOver manager = do
-  showUsage
-  input <- fmap (Prelude.map read.(Prelude.words)) getLine
+  hSetBuffering stdin LineBuffering
+  input <- Prelude.words <$> getLine --fmap (Prelude.map read.(Prelude.words)) getLine
+  putStrLn $ show input
   case input of
-    ["cust", "register", username, password] -> do 
+    ["cust", "register", username, password] -> do
       let usr = requestUser username password
-      -- again VERY BAD as i add empty token "" instead of masking
-      -- it in buildPOSTRequest
-      buildPOSTRequest manager register "" usr
+      req <- buildPOSTRequest register emptyToken usr
+      res <- getResponse req manager
+      L8.putStrLn $ res
       loopOver manager
-    ["cust", "login", username, password] -> do 
+    ["cust", "login", username, password] -> do
       let usr = requestUser username password
-      -- should modify in order to write to file the token
-      -- i am really bad
-      buildPOSTRequest manager login "" usr
+      req <- buildPOSTRequest login emptyToken usr
+      res <- getResponse req manager
+      L8.putStrLn $ res
+      L8.writeFile tokenPath res
       loopOver manager
     ["cust", "new", name, email, phone] -> do
       let customer = requestCustomer name email phone
-      -- i really hate my life
-      -- must implement readToken
       let tkn = readToken tokenPath
-      buildPOSTRequest manager customers customer
+      req <- buildPOSTRequest customers tkn customer
+      res <- getResponse req manager
+      L8.putStrLn $ res
       loopOver manager
     -- this other 2 should be in 
-    ["cust", "list"] -> do putStrLn "Ok Bye!"
-    ["cust", "search", str] -> do putStrLn "Ok Bye!"
+    ["cust", "list"] -> do
+      let tkn = readToken tokenPath
+      req <- buildGETRequest customers tkn
+      res <- getResponse req manager
+      L8.putStrLn $ res      
+      loopOver manager
+    ["cust", "search", str] -> do
+      let tkn = readToken tokenPath
+      let cSearch = customerSearch str
+      req <- buildGETRequest cSearch tkn
+      res <- getResponse req manager
+      L8.putStrLn $ res
+      loopOver manager
     ["quit"] -> do putStrLn "Ok Bye!"
-    _ -> do
+    ["help"] -> do
       showUsage
+      loopOver manager
+
+    _ -> do
+      putStrLn $ "Invalid command"
       loopOver manager
 
 
@@ -251,3 +195,11 @@ showUsage =
     putStrLn "\"cust\" \"search\" \"<string>\""
     putStrLn "\"quit\""
     putStrLn "\n\n"
+
+
+main :: IO ()
+main = do
+  showUsage
+  manager <- newManager tlsManagerSettings
+  loopOver manager
+  putStrLn $ "Ok Bye."
