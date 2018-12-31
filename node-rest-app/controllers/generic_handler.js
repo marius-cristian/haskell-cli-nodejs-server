df926efda273326d2d05d39648b66d9f990349e9
Mikkel Petersen <mlp2305@gmail.com>
2018-12-31 17:31:13 +0100

    more commnets

--- node-rest-app/controllers/generic_handler.js
+++ node-rest-app/controllers/generic_handler.js
@@ -1,8 +1,9 @@
 //MIKKEL any reason for the solver function ? I see you use
 //it for postprocessing the result..this could be made this could be made shorter
- 
+
 exports.generic_handler = (req,res,next,promise,solver)=>{
   promise.then(function(result){
+    //Why this ? 
     let df = null
     df = solver.call(this, result)
     res.json({status:200, message:"Ok.", datafield:df});
