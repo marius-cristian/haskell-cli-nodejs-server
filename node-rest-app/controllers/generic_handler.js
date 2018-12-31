631141153f24d6c4dd97a56f5641f5d2ad743acc
Cristian Marius <cristian.marius.florin@gmail.com>
2018-12-31 18:14:56 +0100

    removed the let df= null; will let it be undefined if function does not return

--- node-rest-app/controllers/generic_handler.js
+++ node-rest-app/controllers/generic_handler.js
@@ -3,9 +3,7 @@
 
 exports.generic_handler = (req,res,next,promise,solver)=>{
   promise.then(function(result){
-    //Why this ? 
-    let df = null
-    df = solver.call(this, result)
+    let df = solver.call(this, result)
     res.json({status:200, message:"Ok.", datafield:df});
   }).catch(function(err){next(err);});
 };
